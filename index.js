c2eedf23fffd63553dab97144eaba93c54880261
David R. Poindexter III <drpoindexter@gmail.com>
2017-01-27 14:03:25 -0500

    Initial checkpoint building queries

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-console.log('\033[2J');
+// console.log('\033[2J');
 
+const Query = require('graphql-query-builder');
 
+const documents = new Query('documents', { category: '510k' });
+const category = new Query('category');
+const currentRevision = new Query('currentRevision');
+
+// category.find(['displayName']);
+// currentRevision.find(category);
+documents.find(['name', currentRevision.find(category.find(['displayName']))]);
+
+const res = `{` + `${documents}` + `}`;
+console.log(res);
 
 // Desired Result
 // `{
